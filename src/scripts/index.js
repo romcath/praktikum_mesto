4523ef6b0f0a99e8723355eabc3eae89c0b80994
Kate <54095868+romcath@users.noreply.github.com>
2020-02-21 22:58:45 +0300

    Changed server IP address for dev and build

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,6 +11,7 @@ import PopupImage from '../blocks/popup/popupImage';
 import PopupProfile from '../blocks/popup/popupProfile';
 import FormValidator from '../scripts/formValidator';
 
+// Переменные
 const userInfo = document.querySelector('.user-info');
 const root = document.querySelector('.root');
 const placesList = document.querySelector('.places-list');
@@ -25,13 +26,19 @@ const words = {
   validationLink: 'Здесь должна быть ссылка'
 }
 
+const serverUrl =
+  NODE_ENV === "development"
+    ? 'http://praktikum.tk/cohort7'
+    : 'https://praktikum.tk/cohort7';
+
 const api = new Api({
-  baseUrl: 'http://95.216.175.5/cohort7',
+  baseUrl: serverUrl,
   headers: {
     authorization: '5547747b-c129-4e77-93a7-481a2a2f0413',
     'Content-Type': 'application/json'
   }
 });
+
 // Инициализация классов
 const validation = new FormValidator(words);
 const popup = new Popup();
@@ -47,144 +54,137 @@ listeners();
 
 function listeners() {
 
-// Загружает первоначальные карточки с сервера
+  // Загружает первоначальные карточки с сервера
   function getCards() {
-      api.getInitialCards().then((result) => {
-        cardList.render(document.querySelector('.places-list'), result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-
-    // Обработчик открытия попапа
-    function openPopup() {
-      if (event.target.classList.contains('user-info__button-edit')) {
-        popupProfile.open();
-      } else if (event.target.classList.contains('user-info__button-add')) {
-        popupCard.open();
-      } else if (event.target.classList.contains('user-info__photo')) {
-        popupAvatar.open();
-      }
-    }
-
-    // Обработчик закрытия попапа
-    function popupClose() {
-      if (event.target.classList.contains('popup__close')) {
-        const closeElement = event.target.closest('.popup');
-        popup.close(closeElement);
-      }
-    }
-
-    // Обработчик событий для списка карточек
-    function cardsList() {
-      if (event.target.classList.contains('place-card__like-icon_liked')) {
-        const getElement = event.target.closest('.place-card');
-
-        api.deleteLike(getElement.id).then((res) => {
-          card.like(res, getElement);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-
-      } else if (event.target.classList.contains('place-card__like-icon')) {
-        const getElement = event.target.closest('.place-card');
-
-        api.putLike(getElement.id).then((res) => {
-          card.like(res, getElement);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-
-      } else if (event.target.classList.contains('place-card__delete-icon')) {
-        const getElement = event.target.closest('.place-card');
-        if (window.confirm('Вы действительно хотите удалить эту карточку?')) {
-            api.deleteCard(getElement.id).then(() => {
-              card.remove(getElement);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-          }
-      } else if (event.target.classList.contains('place-card__image')) {
-        popupImage.openImage();
-      }
-    }
-
-    // Обработчик закрытия попапа по ESC
-    function escKey() {
-      if (event.keyCode === 27) {
-        if (event.target.classList.contains('user-info__button-edit')) {
-          const closeElement = document.querySelector('.popup_type_edit-profile');
-          popup.close(closeElement);
-        } else if (event.target.classList.contains('user-info__button-add')) {
-          const closeElement = document.querySelector('.popup_type_new-card');
-          popup.close(closeElement);
-        }
-      }
-    }
-
-    // Обработчик добавления новой карточки
-    function appendCard() {
-      event.preventDefault();
-
-      const closeElement = event.target.closest('.popup');
-      const item = {
-        name: newCardForm.elements.title.value,
-        link: newCardForm.elements.link.value
-      }
-
-      api.postNewCard(newCardForm.elements.title.value, newCardForm.elements.link.value).then((result) => {
-        item._id = result._id;
-        card.create(item);
-        cardList.addCard(document.querySelector('.places-list'));
-        popup.close(closeElement);
-        newCardForm.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-
-    // Обработчик редактирования профиля
-    function editProfile() {
-      event.preventDefault();
-
-      const closeElement = event.target.closest('.popup');
-      user.setUserInfo(editProfileForm.elements.name.value, editProfileForm.elements.about.value).then(() => {
-        popup.close(closeElement);
-        editProfileForm.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-
-    function avatarChange() {
-      event.preventDefault();
-
-      const closeElement = event.target.closest('.popup');
-      api.patchAvatar(avatarForm.elements.avatar.value).then(() => {
-        user.getUserInfo();
-        popup.close(closeElement);
-        avatarForm.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-
-    // Слушатели событий
-    userInfo.addEventListener('click', openPopup);
-    root.addEventListener('click', popupClose);
-    placesList.addEventListener('click', cardsList);
-    root.addEventListener('keydown', escKey);
-    newCardForm.addEventListener('submit', appendCard);
-    editProfileForm.addEventListener('submit', editProfile);
-    avatarForm.addEventListener('submit', avatarChange);
-
-    // Вызов функций
-    getCards();
+    api.getInitialCards().then((result) => {
+      cardList.render(document.querySelector('.places-list'), result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
+
+  // Обработчик открытия попапа
+  function openPopup() {
+    if (event.target.classList.contains('user-info__button-edit')) {
+      popupProfile.open();
+    } else if (event.target.classList.contains('user-info__button-add')) {
+      popupCard.open();
+    } else if (event.target.classList.contains('user-info__photo')) {
+      popupAvatar.open();
+    }
+  }
+
+  // Обработчик закрытия попапа
+  function popupClose() {
+    if (event.target.classList.contains('popup__close')) {
+      const closeElement = event.target.closest('.popup');
+      popup.close(closeElement);
+    }
+  }
+
+  // Обработчик событий для списка карточек
+  function cardsList() {
+    if (event.target.classList.contains('place-card__like-icon_liked')) {
+      const getElement = event.target.closest('.place-card');
+      api.deleteLike(getElement.id).then((res) => {
+        card.like(res, getElement);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } else if (event.target.classList.contains('place-card__like-icon')) {
+      const getElement = event.target.closest('.place-card');
+      api.putLike(getElement.id).then((res) => {
+        card.like(res, getElement);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } else if (event.target.classList.contains('place-card__delete-icon')) {
+      const getElement = event.target.closest('.place-card');
+      if (window.confirm('Вы действительно хотите удалить эту карточку?')) {
+          api.deleteCard(getElement.id).then(() => {
+            card.remove(getElement);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        }
+    } else if (event.target.classList.contains('place-card__image')) {
+      popupImage.openImage();
+    }
+  }
+
+  // Обработчик закрытия попапа по ESC
+  function escKey() {
+    if (event.keyCode === 27) {
+      if (event.target.classList.contains('user-info__button-edit')) {
+        const closeElement = document.querySelector('.popup_type_edit-profile');
+        popup.close(closeElement);
+      } else if (event.target.classList.contains('user-info__button-add')) {
+        const closeElement = document.querySelector('.popup_type_new-card');
+        popup.close(closeElement);
+      }
+    }
+  }
+
+  // Обработчик добавления новой карточки
+  function appendCard() {
+    event.preventDefault();
+    const closeElement = event.target.closest('.popup');
+    const item = {
+      name: newCardForm.elements.title.value,
+      link: newCardForm.elements.link.value
+    }
+    api.postNewCard(newCardForm.elements.title.value, newCardForm.elements.link.value).then((result) => {
+      item._id = result._id;
+      card.create(item);
+      cardList.addCard(document.querySelector('.places-list'));
+      popup.close(closeElement);
+      newCardForm.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  // Обработчик редактирования профиля
+  function editProfile() {
+    event.preventDefault();
+    const closeElement = event.target.closest('.popup');
+    user.setUserInfo(editProfileForm.elements.name.value, editProfileForm.elements.about.value).then(() => {
+      popup.close(closeElement);
+      editProfileForm.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  // Обработчик изменения аватара
+  function avatarChange() {
+    event.preventDefault();
+    const closeElement = event.target.closest('.popup');
+    api.patchAvatar(avatarForm.elements.avatar.value).then(() => {
+      user.getUserInfo();
+      popup.close(closeElement);
+      avatarForm.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  // Слушатели событий
+  userInfo.addEventListener('click', openPopup);
+  root.addEventListener('click', popupClose);
+  placesList.addEventListener('click', cardsList);
+  root.addEventListener('keydown', escKey);
+  newCardForm.addEventListener('submit', appendCard);
+  editProfileForm.addEventListener('submit', editProfile);
+  avatarForm.addEventListener('submit', avatarChange);
+  
+  // Вызов функций
+  getCards();
+}
