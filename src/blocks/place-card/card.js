d5fd3c8a2eb1d9d7906b388ab0ca6278079d5a2e
Kate <54095868+romcath@users.noreply.github.com>
2021-01-04 15:01:31 +0300

    fix: update place-card block

--- src/blocks/place-card/card.js
+++ src/blocks/place-card/card.js
@@ -24,12 +24,12 @@ export default class Card {
   template() {
     this.cardElement.insertAdjacentHTML('beforeend', `
       <div class='place-card__image'>
-        <button class='place-card__delete-icon'></button>
+        <button type="button" class='place-card__delete-icon'></button>
       </div>
       <div class='place-card__description'>
         <h3 class='place-card__name'></h3>
         <div class='place-card__like'>
-          <button class='place-card__like-icon'></button>
+          <button type="button" class='place-card__like-icon'></button>
           <span class='place-card__like-counter'>0</span>
         </div>
       </div>`);
